a8495d71241ead6a769e0c8877b44ba58f2ea0b5
LadishDev <developer@ladish.dev>
2023-12-16 13:12:23 +0000

    Starter Project Code
    Added Start Project code for my chosen api.

--- src/ofApp.cpp
+++ src/ofApp.cpp
@@ -1,61 +1,55 @@
 #include "ofApp.h"
 
 //--------------------------------------------------------------
-void ofApp::setup(){
+void ofApp::setup() {
+    /*
+    API requires sign up here: https://accounts.discogs.com/login
+    Once signed in you will need to create an app: https://www.discogs.com/settings/developers
+    After creating an app you can generate a personal access token from this page: https://www.discogs.com/settings/developers
+    As the API requires setting of headers to the steps to retrieve data are slightly different to the other APIs
+    */
+    req.headers["User-Agent"] = "450340_MusicAPI_BSU/v1 (callum.baldwin22@bathspa.ac.uk)";//sets user agent header - used to indentify your app
+    req.method = ofHttpRequest::GET;//sets request method to get (e.g. retrieve data)
+    req.url = "https://api.discogs.com/artists/125246?token=mTFAawaxdtKqkgVeBZovKdEwJKjUeLrKBJGPEvcU"; //specify the API call - this will retrieve Nirvana
+
+    res = loader.handleRequest(req);//load request into response object
+    json.parse(res.data);//parse response data into json object so we can work with it
+
+    cout << json.getRawString() << endl;//output raw data
+}
+
+//--------------------------------------------------------------
+void ofApp::update() {
 
 }
 
 //--------------------------------------------------------------
-void ofApp::update(){
+void ofApp::draw() {
+
+	// everything here
+
+	
+
+	
 
 }
 
 //--------------------------------------------------------------
-void ofApp::draw(){
+void ofApp::keyPressed(int key) {
 
 }
 
 //--------------------------------------------------------------
-void ofApp::keyPressed(int key){
+void ofApp::keyReleased(int key) {
+
+}
+
+//-----------------------------------------------------
+void ofApp::mousePressed(int x, int y, int button) {
 
 }
 
 //--------------------------------------------------------------
-void ofApp::keyReleased(int key){
-
-}
-
-//--------------------------------------------------------------
-void ofApp::mousePressed(int x, int y, int button){
-
-}
-
-//--------------------------------------------------------------
-void ofApp::mouseReleased(int x, int y, int button){
-
-}
-
-//--------------------------------------------------------------
-void ofApp::mouseEntered(int x, int y){
-
-}
-
-//--------------------------------------------------------------
-void ofApp::mouseExited(int x, int y){
-
-}
-
-//--------------------------------------------------------------
-void ofApp::windowResized(int w, int h){
-
-}
-
-//--------------------------------------------------------------
-void ofApp::gotMessage(ofMessage msg){
-
-}
-
-//--------------------------------------------------------------
-void ofApp::dragEvent(ofDragInfo dragInfo){ 
+void ofApp::mouseReleased(int x, int y, int button) {
 
 }
--- src/ofApp.h
+++ src/ofApp.h
@@ -1,22 +1,24 @@
 #pragma once
 
 #include "ofMain.h"
+#include "ofxJSON.h"
+#include <vector>
 
-class ofApp : public ofBaseApp{
+class ofApp : public ofBaseApp {
 
-	public:
-		void setup();
-		void update();
-		void draw();
+public:
+	void setup();
+	void update();
+	void draw();
 
-		void keyPressed(int key);
-		void keyReleased(int key);
-		void mousePressed(int x, int y, int button);
-		void mouseReleased(int x, int y, int button);
-		void mouseEntered(int x, int y);
-		void mouseExited(int x, int y);
-		void windowResized(int w, int h);
-		void dragEvent(ofDragInfo dragInfo);
-		void gotMessage(ofMessage msg);
-		
+	void keyPressed(int key);
+	void keyReleased(int key);
+	void mousePressed(int x, int y, int button);
+	void mouseReleased(int x, int y, int button);
+
+	ofHttpRequest req;//handles request to API
+	ofHttpResponse res;//handles response from API
+	ofURLFileLoader loader;//object to load data
+	ofxJSONElement json;//object to store data
+
 };
